0292cdde23247e84d88158d36e08ab8d2dbec4a2
Wesley <wesleyvc2110@gmail.com>
2023-05-17 15:33:56 -0300

    feat: create route without to test

--- src/app.js
+++ src/app.js
@@ -1,24 +1,29 @@
 const express = require('express');
+
 const app = express();
 app.use(express.json());
 
 const Tarefas = require('./tarefas');
 const tarefas = new Tarefas();
 
-app.post('/tarefas', (req, res) => {
-
+app.post('/tarefas', async (req, res) => {
+    await Tarefas.adicionar(req.body.descricao);
+    return res.status(201).json('Tarefa adicionada com sucesso!');
 });
 
-app.delete('/tarefas/:indice', (req, res) => {
-
+app.delete('/tarefas/:indice', async (req, res) => {
+    await Tarefas.remover(req.params.indice);
+    return res.status(200).json('Tarefa removida com sucesso!');
 });
 
-app.get('/tarefas', (req, res) => {
-
+app.get('/tarefas', async (req, res) => {
+    const response = await Tarefas.todas();
+    return res.status(200).json(response);
 });
 
-app.patch('/tarefas/:indice', (req, res) => {
-
+app.patch('/tarefas/:indice', async (req, res) => {
+    await this.Tarefas.marcarConcluida(req.params.indice);
+    return res.status(200).json('Tarefa marcada como concluída!');
 });
 
 module.exports = app;
--- src/tarefas.js
+++ src/tarefas.js
@@ -4,19 +4,28 @@ class Tarefas {
     }
   
     adicionar(descricao) {
-      
+      this.lista.push({descricao, concluida: false});
+      return;
     }
   
     remover(indice) {
-      
+      if(indice > this.lista.length || indice < 0){
+        return `A lista atual vai de 0 à ${this.lista.length} elementos.`
+      }
+      this.lista.splice(indice, 1);
+      return;
     }
   
     todas() {
-      
+      return this.lista;
     }
   
     marcarConcluida(indice) {
-     
+      if(indice > this.lista.length || indice < 0){
+        return `A lista atual vai de 0 à ${this.lista.length} elementos.`
+      }
+      this.lista[indice].concluida = true;
+      return;
     }
   }
   
